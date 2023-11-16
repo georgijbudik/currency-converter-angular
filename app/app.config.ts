47347c21eb201c2786ba0b419b561850275189c2
georgijbudik <georgijbudik@gmail.com>
2023-11-16 22:32:09 +0100

    Auto-generated commit [ci skip]

--- app/app.config.ts
+++
@@ -1,9 +0,0 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
-import { FormControl } from '@angular/forms';
-
-export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(), FormControl],
-};
--- app/models/Rate.ts
+++
@@ -1,3 +0,0 @@
-export class Rate {
-  constructor(public txt: string, public rate: number, public cc: string) {}
-}
